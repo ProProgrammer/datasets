f091438dba051ca00da3422821b450504713b152
Deep Sukhwani <deepsukhwani@gmail.com>
2020-12-13 03:51:05 +0530

    DatasetModelViewSet: override `retrieve` for custom response
    Custom response should return file name and file size

--- dataset/views.py
+++ dataset/views.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework.status import HTTP_200_OK
 
 from dataset.models import Dataset
 from dataset.serializers import DatasetModelSerializer
@@ -11,3 +13,17 @@ class DatasetModelViewSet(viewsets.ModelViewSet):
     queryset = Dataset.objects.all()
     serializer_class = DatasetModelSerializer
     pagination_class = None
+
+    def retrieve(self, request, *args, **kwargs):
+        """
+        Override retrieve to return only filename and size of the dataset object
+        """
+
+        dataset_object: Dataset = self.get_object()
+
+        custom_response = {
+            'file': dataset_object.file.name,
+            'size': f'{dataset_object.file.size} bytes'
+        }
+
+        return Response(custom_response, status=HTTP_200_OK)
